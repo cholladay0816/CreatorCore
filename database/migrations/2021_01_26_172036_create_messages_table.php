2ee537142b7213ec47028f2412397d0ba33ea516
Chris H <cholladay0816@gmail.com>
2021-01-26 11:31:48 -0600

    Messages

---
+++ database/migrations/2021_01_26_172036_create_messages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sender_id');
+            $table->foreign('sender_id')->references('id')->on('users')
+                ->cascadeOnDelete();
+            $table->foreignId('receiver_id');
+            $table->foreign('receiver_id')->references('id')->on('users')
+                ->cascadeOnDelete();
+            $table->string('message', 2048);
+            $table->dateTime('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
