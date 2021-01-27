9028b454b4d479623c3fb1aec13ec24389fdbece
Chris H <cholladay0816@gmail.com>
2021-01-26 20:48:39 -0600

    Updated Composer.Lock

--- tests/Feature/CommissionFeatureTest.php
+++ tests/Feature/CommissionFeatureTest.php
@@ -6,7 +6,6 @@ use App\Models\Attachment;
 use App\Models\Commission;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CommissionFeatureTest extends TestCase
