bc1ff8effc9f4760c55968acb9fef15c07cf5097
Chris H <cholladay0816@gmail.com>
2021-01-31 20:09:31 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AttachmentController;
 use App\Http\Controllers\BankAccountController;
 use App\Http\Controllers\CommissionController;
+use App\Http\Controllers\SourceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,16 +26,24 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
         return view('dashboard');
     })->name('dashboard');
 
+    Route::get('source/new', [SourceController::class, 'create'])
+        ->name('source.create');
+    Route::post('source/new', [SourceController::class, 'store'])
+        ->name('source.store');
+
     Route::post('/attachments/new/{commission}', [AttachmentController::class, 'store'])
         ->name('attachments.store');
 
     Route::delete('/attachments/{attachment}', [AttachmentController::class, 'destroy'])
         ->name('attachments.destroy');
 
-    Route::get('/commissions/new/{user}/{commissionpreset?}', [CommissionController::class, 'create'])
-        ->name('commissions.create');
-    Route::post('/commissions/new/{user}/{commissionpreset?}', [CommissionController::class, 'store'])
-        ->name('commissions.store');
+    Route::middleware(['paymentmethod'])->group(function () {
+        Route::get('/commissions/new/{user}/{commissionpreset?}', [CommissionController::class, 'create'])
+            ->name('commissions.create');
+        Route::post('/commissions/new/{user}/{commissionpreset?}', [CommissionController::class, 'store'])
+            ->name('commissions.store');
+    });
+
     Route::get('/commissions/{commission}', [CommissionController::class, 'show'])
         ->name('commissions.show');
 
