cb38b868043b8c247986d2360e91f3bb2c692354
Chris H <cholladay0816@gmail.com>
2021-02-11 16:43:46 -0600

    TearDown Generated Stripe Data

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,27 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Stripe\StripeClient;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function tearDown(): void
+    {
+        $users = User::all();
+        foreach ($users as $user) {
+            if ($user->stripe_id) {
+                $stripe = new StripeClient(config('stripe.secret'));
+                $stripe->customers->delete($user->stripe_id);
+            }
+            if ($user->stripe_account_id) {
+                $stripe = new StripeClient(config('stripe.secret'));
+                $stripe->accounts->delete($user->stripe_account_id);
+            }
+        }
+        parent::tearDown();
+    }
 }
