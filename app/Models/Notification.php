38aca576b15a45f425e7f58288baa17f07035c5a
Christian Holladay <cholladay0816@gmail.com>
2021-02-24 12:40:12 -0600

    Update Notification.php

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+    
+    protected $dates = ['created_at', 'updated_at', 'read_at'];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
