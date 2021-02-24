b0478a699667b550ad672b2c1117d09b1c8e43ba
Christian Holladay <cholladay0816@gmail.com>
2021-02-24 12:40:18 -0600

    Create 2021_02_24_182820_create_notifications_table.php

---
+++ database/migrations/2021_02_24_182820_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class)->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('description');
+            $table->string('url')->nullable();
+            $table->dateTime('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
