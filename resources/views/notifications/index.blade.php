49b0ff70cabdc3ee603bc57e2f700cd3ebb26815
Christian Holladay <cholladay0816@gmail.com>
2021-02-24 17:30:37 -0600

    Update index.blade.php

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    @if(isset($notifications))
+    @if(count($notifications))
         <div>
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 @livewire('notifications', ['notifications' => $notifications])
