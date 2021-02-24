ac0aab410d5841b2a3e4356128bef5b23e2242dc
Christian Holladay <cholladay0816@gmail.com>
2021-02-24 15:28:04 -0600

    Update index.blade.php

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -1,15 +1,14 @@
 <x-app-layout>
-    @if(count($notifications))
-    @foreach($notifications as $notification)
-        <span>{{$notification->title}}</span>
-    @endforeach
-
+    @if(isset($notifications))
+        <div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                @livewire('notifications', ['notifications' => $notifications])
+            </div>
+        </div>
     @else
-
         <div class="min-h-screen flex">
             <div class="pointer-events-none my-auto mx-auto font-light text-gray-500 text-2xl">You're all caught up!</div>
         </div>
-
     @endif
 
 </x-app-layout>
