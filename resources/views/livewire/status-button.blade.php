96e4cda296ef3a2debef171458268b2c97591a0b
Christian Holladay <cholladay0816@gmail.com>
2021-02-24 17:30:45 -0600

    Create status-button.blade.php

---
+++ resources/views/livewire/status-button.blade.php
@@ -0,0 +1,5 @@
+<div class="relative inline-flex items-center pr-2 py-1 text-sm">
+    <span class="absolute flex-shrink-0 flex items-center justify-center">
+        <span class="h-1.5 w-1.5 rounded-full {{$color ?? ''}}" aria-hidden="true"></span>
+    </span>
+</div>
