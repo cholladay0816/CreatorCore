fc780b70778ef3c5052157369014df757aa01f42
Christian Holladay <cholladay0816@gmail.com>
2021-02-24 15:46:26 -0600

    Update NotificationController.php

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -21,46 +21,4 @@ class NotificationController extends Controller
         $notifications = auth()->user()->notifications;
         return view('notifications.index', ['notifications' => $notifications]);
     }
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param Notification $notification
-     * @return Application|Factory|View|Response
-     */
-    public function show(Notification $notification)
-    {
-        return view('notifications.show', ['notification' => $notification]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param Notification $notification
-     * @return Response
-     */
-    public function update(Request $request, Notification $notification): Response
-    {
-        if ($notification->read_at != null || $notification->user->id != auth()->id()) {
-            abort(401);
-        }
-        $notification->read_at = now();
-        $notification->save();
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Notification $notification
-     * @return Response
-     */
-    public function destroy(Notification $notification): Response
-    {
-        if ($notification->user->id != auth()->id()) {
-            abort(401);
-        }
-        $notification->delete();
-    }
 }
