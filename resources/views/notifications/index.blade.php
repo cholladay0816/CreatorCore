a1005745d017870e6e6fa6dae413fcbbfe289714
Christian Holladay <cholladay0816@gmail.com>
2021-02-24 13:40:38 -0600

    Update index.blade.php

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -1,7 +1,15 @@
 <x-app-layout>
-
+    @if(count($notifications))
     @foreach($notifications as $notification)
         <span>{{$notification->title}}</span>
     @endforeach
+
+    @else
+
+        <div class="min-h-screen flex">
+            <div class="pointer-events-none my-auto mx-auto font-light text-gray-500 text-2xl">You're all caught up!</div>
+        </div>
+
+    @endif
 
 </x-app-layout>
