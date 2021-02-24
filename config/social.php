adb9be1d69a0821ba3127444bbbdd0c78343f211
Christian Holladay <cholladay0816@gmail.com>
2021-02-24 13:40:21 -0600

    Create social.php

---
+++ config/social.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'facebook' => env('SOCIAL_FACEBOOK', ''),
+    'instagram' => env('SOCIAL_INSTAGRAM', ''),
+    'twitter' => env('SOCIAL_TWITTER', ''),
+    'github' => env('SOCIAL_GITHUB', '')
+];
