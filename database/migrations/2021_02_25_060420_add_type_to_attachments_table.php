f31c4556f640d9481310cbb18cb50ba7820467bf
Christian Holladay <cholladay0816@gmail.com>
2021-02-25 00:19:59 -0600

    Create 2021_02_25_060420_add_type_to_attachments_table.php

---
+++ database/migrations/2021_02_25_060420_add_type_to_attachments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTypeToAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('attachments', function (Blueprint $table) {
+            $table->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('attachments', function (Blueprint $table) {
+            $table->removeColumn('type');
+        });
+    }
+}
