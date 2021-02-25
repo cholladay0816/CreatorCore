338b51e4df17141db351c99e84c81f9f5511c4e0
Christian Holladay <cholladay0816@gmail.com>
2021-02-25 16:21:28 -0600

    Update attachments.blade.php

--- resources/views/livewire/commission/attachments.blade.php
+++ resources/views/livewire/commission/attachments.blade.php
@@ -1,21 +1,25 @@
-<div x-data="{ isUploading: false}" x-on:livewire-upload-start="isUploading = true" class="mt-1 sm:mt-0 sm:col-span-2">
-    <div class="flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-        <div class="space-y-1 text-center" x-show="!isUploading" >
+<div x-data="{ isUploading: false, hasErrors: @entangle('hasErrors')}" x-on:livewire-upload-start="isUploading = true" class="mt-1 sm:mt-0 sm:col-span-2">
+    <div class="flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md relative">
+        <input
+            wire:model="file"
+            id="file-upload" name="file-upload" type="file" class="absolute inset-0 z-50 m-0 p-0 w-full h-full outline-none opacity-0"
+        >
+
+        <div class="space-y-1 text-center" x-show="!isUploading || hasErrors" >
             <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                 <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
             </svg>
             <div class="flex text-sm text-gray-600">
                 <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-lightBlue-600 hover:text-lightBlue-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-lightBlue-500">
                     <span>Upload a file</span>
-                    <input wire:model="file" id="file-upload" name="file-upload" type="file" class="sr-only">
-                </label>
+                    </label>
                 <p class="pl-1">or drag and drop</p>
             </div>
             <p class="text-xs text-gray-500">
                 PNG, JPG, GIF up to 4MB
             </p>
         </div>
-        <div class="space-y-1 text-center" x-show="isUploading">
+        <div class="space-y-1 text-center" x-show="isUploading && !hasErrors">
             <div>
                 <span class="text-lightBlue-400 inline-flex items-center border border-transparent text-base leading-6 font-medium rounded-md transition ease-in-out duration-150">
                     File uploading...
@@ -28,6 +32,7 @@
         {{$message}}
     </p>
     @enderror
+    @if(count($commission->attachments))
     <dt class="pt-4 text-sm font-medium text-gray-500">
         Attachments
     </dt>
@@ -48,10 +53,10 @@
                         <a target="_blank" href="{{route('attachments.show', $attachment)}}" class="font-medium text-indigo-600 hover:text-indigo-500">
                             View
                         </a>
-                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                        <a href="{{route('attachments.show', $attachment)}}" download class="font-medium text-indigo-600 hover:text-indigo-500">
                             Download
                         </a>
-                        <a @click.prevent="isVisible = false, $wire.call('delete',{{$attachment->id}})" href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                        <a @click.prevent="isVisible = false, $wire.call('delete',{{$attachment->id}})" class="cursor-pointer font-medium text-indigo-600 hover:text-indigo-500">
                             Delete
                         </a>
                     </div>
@@ -59,4 +64,5 @@
             @endforeach
         </ul>
     </dd>
+    @endif
 </div>
