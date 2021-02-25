a2265308975be60d70a43b2cddeb612da0b9b867
Christian Holladay <cholladay0816@gmail.com>
2021-02-24 20:18:27 -0600

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,6 +59,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'verified.account' => Middleware\RequireVerifiedStripeAccount::class,
+        'verified.payment' => Middleware\RequireVerifiedPaymentMethod::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
