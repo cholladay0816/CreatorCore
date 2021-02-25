c4a614ae0d75dc31bca67a234dced3999fc82953
Christian Holladay <cholladay0816@gmail.com>
2021-02-25 15:30:06 -0600

    Update action-buttons.blade.php

--- resources/views/livewire/commission/action-buttons.blade.php
+++ resources/views/livewire/commission/action-buttons.blade.php
@@ -22,20 +22,6 @@
             </button>
         </form>
     @endif
-
-    @if($commission->status == 'Pending' && $commission->buyer->id == auth()->id())
-            <form class="inline-flex items-center" method="POST" action="{{route('commissions.destroy', $commission)}}">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-rose-600 hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
-                    <svg class="-ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                    </svg>
-                    Cancel
-                </button>
-            </form>
-    @endif
-
         @if($commission->status == 'Pending' && $commission->creator->id == auth()->id())
             <form class="inline-flex items-center" method="POST" action="{{route('commissions.update', $commission)}}">
                 @csrf
@@ -58,7 +44,7 @@
                 </button>
             </form>
         @endif
-        @if($commission->status == 'Active' && $commission->creator->id == auth()->id())
+        @if($commission->status == 'Active' && $commission->creator->id == auth()->id() && count($commission->attachments) > 0)
             <form class="inline-flex items-center" method="POST" action="{{route('commissions.update', $commission)}}">
                 @csrf
                 @method('PUT')
