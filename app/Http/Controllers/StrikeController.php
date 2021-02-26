fb78177c2ab3fd102b5e0c0e6210cec92539f67c
Christian Holladay <cholladay0816@gmail.com>
2021-02-25 19:30:18 -0600

    Create StrikeController.php

---
+++ app/Http/Controllers/StrikeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Strike;
+use Illuminate\Http\Request;
+
+class StrikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Strike  $strike
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Strike $strike)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Strike  $strike
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Strike $strike)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Strike  $strike
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Strike $strike)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Strike  $strike
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Strike $strike)
+    {
+        //
+    }
+}
