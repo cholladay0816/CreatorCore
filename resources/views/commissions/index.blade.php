c139c6b7841d298fc0f2163f9de2dc93e57d2791
Christian Holladay <cholladay0816@gmail.com>
2021-02-25 19:22:34 -0600

    Update index.blade.php

--- resources/views/commissions/index.blade.php
+++ resources/views/commissions/index.blade.php
@@ -30,16 +30,29 @@
                                             ${{number_format($commission->price,2)}}
                                         </p>
                                     </div>
-                                    <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                                        <!-- Heroicon name: solid/calendar -->
-                                        <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-                                        </svg>
-                                        <p>
-                                            Due
-                                            <time datetime="{{$commission->expires_at->format("Y-m-d")}}">{{$commission->expires_at->diffForHumans()}}</time>
-                                        </p>
-                                    </div>
+                                    @if(in_array($commission->status, ['Active', 'Overdue']))
+                                        <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                                            <!-- Heroicon name: solid/calendar -->
+                                            <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+                                            </svg>
+                                            <p>
+                                                Due
+                                                <time datetime="{{$commission->expires_at->format("Y-m-d")}}">{{$commission->expires_at->diffForHumans()}}</time>
+                                            </p>
+                                        </div>
+                                    @else
+                                        <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                                            <!-- Heroicon name: solid/calendar -->
+                                            <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+                                            </svg>
+                                            <p>
+                                                Time to Complete:
+                                                <time datetime="{{now()->addDays($commission->days_to_complete)->format("Y-m-d")}}">{{$commission->days_to_complete}} days</time>
+                                            </p>
+                                        </div>
+                                    @endif
                                 </div>
                             </div>
                         </a>
