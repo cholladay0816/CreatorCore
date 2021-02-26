a9a2625d8173b467cfa9eab3057ad9852aaca65f
Christian Holladay <cholladay0816@gmail.com>
2021-02-26 00:40:44 -0600

    Update AttachmentFeatureTest.php

--- tests/Feature/AttachmentFeatureTest.php
+++ tests/Feature/AttachmentFeatureTest.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class AttachmentFeatureTest extends TestCase
@@ -23,12 +24,10 @@ class AttachmentFeatureTest extends TestCase
         // Create a dummy image
         $file = UploadedFile::fake()->image('test.png')->size(4097);
         // Attempt to upload this file as an attachment
-        $this->actingAs($user)
-            ->post(route('attachments.store', $commission), [
-                'file' => $file
-            ])
-            // Assert the file failed to upload.
-            ->assertSessionHasErrors();
+        $this->actingAs($user);
+        Livewire::test('commission.attachments', ['commission' => $commission])
+            ->set('file', $file)
+            ->assertHasErrors();
     }
     /** @test */
     public function an_attachment_cannot_be_uploaded_if_it_is_not_an_image()
@@ -40,30 +39,24 @@ class AttachmentFeatureTest extends TestCase
         // Create a dummy executable
         $file = UploadedFile::fake()->create('test.exe', '1024', 'application/x-msdownload');
         // Attempt to upload this file as an attachment
-        $this->actingAs($user)
-            ->post(route('attachments.store', $commission), [
-                'file' => $file
-            ])
-            // Assert the file failed to upload.
-            ->assertSessionHasErrors();
+        $this->actingAs($user);
+        Livewire::test('commission.attachments', ['commission' => $commission])
+            ->set('file', $file)
+            ->assertHasErrors();
 
         $file = UploadedFile::fake()->create('test.mp4', '1024', 'video/mp4');
         // Attempt to upload this file as an attachment
-        $this->actingAs($user)
-            ->post(route('attachments.store', $commission), [
-                'file' => $file
-            ])
-            // Assert the file failed to upload.
-            ->assertSessionHasErrors();
+        $this->actingAs($user);
+        Livewire::test('commission.attachments', ['commission' => $commission])
+            ->set('file', $file)
+            ->assertHasErrors();
 
         $file = UploadedFile::fake()->create('test.cpp', '12', 'text/plain');
         // Attempt to upload this file as an attachment
-        $this->actingAs($user)
-            ->post(route('attachments.store', $commission), [
-                'file' => $file
-            ])
-            // Assert the file failed to upload.
-            ->assertSessionHasErrors();
+        $this->actingAs($user);
+        Livewire::test('commission.attachments', ['commission' => $commission])
+            ->set('file', $file)
+            ->assertHasErrors();
     }
 
     /** @test */
@@ -77,26 +70,22 @@ class AttachmentFeatureTest extends TestCase
         // Create a dummy image
         $file = UploadedFile::fake()->image('test.png', '64', '64');
         // Attempt to upload this image as an attachment
-        $this->actingAs($user)
-            ->post(route('attachments.store', $commission), [
-                'file' => $file
-            ])
-            // Assert the file was uploaded successfully
-            ->assertSessionHas('success', 'Attachment created')
-            // Assert we are redirected back to the commissions page
+        $this->actingAs($user);
+        Livewire::test('commission.attachments', ['commission' => $commission])
+            ->set('file', $file)
             ->assertRedirect(route('commissions.show', $commission));
+
+        $commission = $commission->fresh();
+
         // Assert we have exactly one commission attachment
         $this->assertEquals(1, $commission->attachments->count());
 
-        // Assert the file was created
-        Storage::assertExists('attachments/' . $file->hashName());
         $attachment = $commission->attachments->first();
-        $path = $attachment->path;
 
-        // Delete the attachment
-        $attachment->delete();
-        // Assert that the file no longer exists
-        Storage::assertMissing($path);
+        $this->actingAs($user);
+        Livewire::test('commission.attachments', ['commission' => $commission])
+            ->call('delete', $attachment->id);
+
         // Assert the attachment is gone
         $this->assertNull($attachment->fresh());
     }
@@ -114,9 +103,10 @@ class AttachmentFeatureTest extends TestCase
             'commission_id' => $commission->id
         ])->fresh();
 
-        $this->actingAs($user)
-            ->delete(route('attachments.destroy', $attachment))
-            ->assertStatus(401);
+        $this->actingAs($user);
+        Livewire::test('commission.attachments', ['commission' => $commission])
+            ->call('delete', $attachment->id);
+        $this->assertNotNull($attachment->fresh());
     }
 
     /** @test */
@@ -134,27 +124,28 @@ class AttachmentFeatureTest extends TestCase
             'commission_id' => $commission->id
         ])->fresh();
 
-        $this->actingAs($buyer)
-            ->delete(route('attachments.destroy', $attachment))
-            ->assertStatus(401);
+        $this->actingAs($buyer);
+        Livewire::test('commission.attachments', ['commission' => $commission])
+            ->call('delete', $attachment->id);
+        $this->assertNotNull($attachment->fresh());
     }
 
     /** @test */
     public function an_attachment_can_be_deleted_by_its_creator()
     {
-        $user = User::factory()->create();
+        $user = User::factory()->create()->fresh();
         $commission = Commission::factory()->create([
             'creator_id' => $user->id,
             'status' => 'Active'
         ])->fresh();
         $attachment = Attachment::factory()->create([
             'user_id' => $user->id,
-            'commission_id' => $commission->id
+            'commission_id' => $commission->id,
+            'path' => 'attachments/test.png'
         ])->fresh();
-
-        $this->actingAs($user)
-            ->delete(route('attachments.destroy', $attachment))
-            ->assertSessionHas('success', 'Attachment deleted')
-            ->assertRedirect(route('commissions.show', $commission));
+        $this->actingAs($user);
+        Livewire::test('commission.attachments', ['commission' => $commission])
+            ->call('delete', $attachment->id);
+        $this->assertNull($attachment->fresh());
     }
 }
