9eab4ac6cd4d529de0163ced53c2c9fc7623193f
Christian Holladay <cholladay0816@gmail.com>
2021-03-26 02:21:45 -0500

    Create 2021_03_25_054037_create_creators_table.php

---
+++ database/migrations/2021_03_25_054037_create_creators_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCreatorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('creators', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('headline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('creators');
+    }
+}
