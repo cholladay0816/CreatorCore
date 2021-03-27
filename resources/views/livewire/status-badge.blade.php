c8869cbd7f483460dd6603e00ab901260fbc13b3
Christian Holladay <cholladay0816@gmail.com>
2021-03-27 17:58:19 -0500

    Update status-badge.blade.php

--- resources/views/livewire/status-badge.blade.php
+++ resources/views/livewire/status-badge.blade.php
@@ -1,5 +1,5 @@
 <div class="ml-2 flex-shrink-0 flex">
-    <p class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-{{$color}}-100 text-{{$color}}-800">
+    <p class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full {{ $bgColor ?? 'bg-gray-100' }} {{ $textColor ?? 'text-gray-800' }} ">
         {{$status}}
     </p>
 </div>
