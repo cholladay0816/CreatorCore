14dd4259288fb8d8e0ef468fe77d68f1b69677f2
Christian Holladay <cholladay0816@gmail.com>
2021-03-27 17:58:06 -0500

    Update StatusBadge.php

--- app/Http/Livewire/StatusBadge.php
+++ app/Http/Livewire/StatusBadge.php
@@ -7,19 +7,23 @@ use Livewire\Component;
 class StatusBadge extends Component
 {
     public $status;
-    public $color;
+    public $bgColor = 'bg-white';
+    public $textColor = 'text-gray-800';
     public function render()
     {
         if (in_array($this->status, ['Unpaid', 'Declined', 'Failed', 'Expired', 'Refunded'])) {
-            $this->color = 'rose';
+            $this->bgColor = 'bg-rose-100';
+            $this->textColor = 'text-rose-800';
         } elseif (in_array($this->status, ['Pending', 'Purchasing', 'Overdue', 'Disputed'])) {
-            $this->color = 'yellow';
+            $this->bgColor = 'bg-yellow-100';
+            $this->textColor = 'text-yellow-800';
         } elseif (in_array($this->status, ['Active', 'Completed'])) {
-            $this->color = 'green';
+            $this->bgColor = 'bg-green-100';
+            $this->textColor = 'text-green-800';
         } else {
-            $this->color = 'blue';
+            $this->bgColor = 'bg-blue-100';
+            $this->textColor = 'text-blue-800';
         }
-
         return view('livewire.status-badge');
     }
 }
