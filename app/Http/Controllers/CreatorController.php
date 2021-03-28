8abdc6730e9a4a02b591c100f9e9256d1655eb42
Christian Holladay <cholladay0816@gmail.com>
2021-03-27 20:46:08 -0500

    Create CreatorController.php

---
+++ app/Http/Controllers/CreatorController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Creator;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CreatorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param User $user
+     * @return Application|Factory|View|Response
+     */
+    public function show(User $user)
+    {
+        abort_if(!$user->isValidCreator(), 404);
+
+        return view('livewire.creator.show', ['user' => $user]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Creator $creator
+     * @return Response
+     */
+    public function edit(Creator $creator)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Creator $creator
+     * @return Response
+     */
+    public function update(Request $request, Creator $creator)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Creator $creator
+     * @return Response
+     */
+    public function destroy(Creator $creator)
+    {
+        //
+    }
+}
