fc50f919887ae28171d2b852cc5db92bfc51631f
Christian Holladay <cholladay0816@gmail.com>
2021-03-27 20:47:26 -0500

    Create Show.php

---
+++ app/Http/Livewire/Creator/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Creator;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.creator.show');
+    }
+}
