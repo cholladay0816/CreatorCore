e2a83c84925d2268c270a02ce53155b3bff633a9
Christian Holladay <cholladay0816@gmail.com>
2021-03-27 20:51:07 -0500

    Create About.php

---
+++ app/Http/Livewire/Creator/About.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Creator;
+
+use Livewire\Component;
+
+class About extends Component
+{
+    public function render()
+    {
+        return view('livewire.creator.about');
+    }
+}
