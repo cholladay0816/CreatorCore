b52e917b0e9325c6d9eebcd35d782751c5bd6cb7
Christian Holladay <cholladay0816@gmail.com>
2021-03-27 19:11:04 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CreatorSeeder::class,
+            RoleSeeder::class,
         ]);
     }
 }
