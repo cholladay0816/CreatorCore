12b859e5f1f75b86dcc401a4abdcc0239f6fefb3
Christian Holladay <cholladay0816@gmail.com>
2021-03-27 20:51:10 -0500

    Create Commissions.php

---
+++ app/Http/Livewire/Creator/Commissions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Creator;
+
+use Livewire\Component;
+
+class Commissions extends Component
+{
+    public function render()
+    {
+        return view('livewire.creator.commissions');
+    }
+}
