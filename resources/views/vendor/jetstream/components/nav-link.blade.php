b6fd60953d2898998943deabe6833eae059de604
Christian Holladay <cholladay0816@gmail.com>
2021-03-27 20:47:33 -0500

    Update nav-link.blade.php

--- resources/views/vendor/jetstream/components/nav-link.blade.php
+++ resources/views/vendor/jetstream/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'
+            : 'text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
