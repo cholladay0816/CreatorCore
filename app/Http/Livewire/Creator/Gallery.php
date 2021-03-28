6f25138814a08ed2e202cb917168586ef9401c18
Christian Holladay <cholladay0816@gmail.com>
2021-03-27 20:51:13 -0500

    Create Gallery.php

---
+++ app/Http/Livewire/Creator/Gallery.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Creator;
+
+use Livewire\Component;
+
+class Gallery extends Component
+{
+    public function render()
+    {
+        return view('livewire.creator.gallery');
+    }
+}
