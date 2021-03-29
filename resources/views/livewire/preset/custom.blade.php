f1af4cb5b256210b35ba78a119153dabb2e524e2
Christian Holladay <cholladay0816@gmail.com>
2021-03-29 01:42:27 -0500

    Create custom.blade.php

---
+++ resources/views/livewire/preset/custom.blade.php
@@ -0,0 +1,13 @@
+<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+    <a href="{{ route('commissions.create', $user) }}"><img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"></a>
+    <div class="px-6 py-4">
+        <a href="{{ route('commissions.create', $user) }}" class="font-bold text-xl mb-2">
+            Create a Custom Commission
+        </a>
+        <p class="text-gray-700 text-base">
+            Click here to send a Custom Commission to this Creator
+        </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+    </div>
+</div>
