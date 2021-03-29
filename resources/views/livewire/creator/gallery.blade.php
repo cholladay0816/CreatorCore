5367c79398cc96b3955287e6e5a05543684169ad
Christian Holladay <cholladay0816@gmail.com>
2021-03-29 01:44:54 -0500

    Update gallery.blade.php

--- resources/views/livewire/creator/gallery.blade.php
+++ resources/views/livewire/creator/gallery.blade.php
@@ -1,3 +1,5 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-15 py-10">
+    @foreach($user->gallery as $gallery)
+        @livewire('gallery.gallery', ['gallery' => $gallery, 'view_as_guest'=>1])
+    @endforeach
 </div>
