c0fb2e5fc18238133cdd007d273be10e0ac619bf
Christian Holladay <cholladay0816@gmail.com>
2021-03-29 01:40:49 -0500

    Create Create.php

---
+++ app/Http/Livewire/Preset/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Preset;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.preset.create');
+    }
+}
