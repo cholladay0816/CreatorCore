8431581a981cf0c7fa2425cc9a17967ce6e8557e
Christian Holladay <cholladay0816@gmail.com>
2021-03-29 01:44:46 -0500

    Update commissions.blade.php

--- resources/views/livewire/creator/commissions.blade.php
+++ resources/views/livewire/creator/commissions.blade.php
@@ -1,3 +1,11 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-15 py-10">
+    @foreach($user->commissionPresets as $preset)
+        @livewire('preset.card', ['preset' => $preset, 'view_as_guest'=>1])
+    @endforeach
+    @if($user->id == auth()->id())
+        @livewire('preset.create')
+    @elseif($user->creator->open && $user->creator->allows_custom_commissions)
+        @livewire('preset.custom', ['creator'=>$creator])
+    @endif
 </div>
