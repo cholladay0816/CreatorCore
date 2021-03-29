2ad76d267215ab9da56d0cafb0bf64d2aab2cb5b
Christian Holladay <cholladay0816@gmail.com>
2021-03-29 18:05:10 -0500

    Create Paginator.php

---
+++ app/Helpers/Paginator.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Helpers;
+
+
+use Illuminate\Support\Collection;
+
+class Paginator
+{
+    public static function paginate($items, $perPage = 15, $page = null, $options = [])
+    {
+        $page = $page ?: (\Illuminate\Pagination\Paginator::resolveCurrentPage() ?: 1);
+
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+
+        return new \Illuminate\Pagination\LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+    }
+}
