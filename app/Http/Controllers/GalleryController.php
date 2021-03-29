5806d1b983b008a20eaa394dbee8f430b0f2fa00
Christian Holladay <cholladay0816@gmail.com>
2021-03-29 01:45:41 -0500

    Create GalleryController.php

---
+++ app/Http/Controllers/GalleryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gallery;
+use Illuminate\Http\Request;
+
+class GalleryController extends Controller
+{
+
+    public function show(Gallery $gallery)
+    {
+        //
+    }
+}
