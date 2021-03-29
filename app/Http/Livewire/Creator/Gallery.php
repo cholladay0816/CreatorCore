1fd5b34894c3ddf52cf32cbfbca19f324b098c0e
Christian Holladay <cholladay0816@gmail.com>
2021-03-28 21:36:53 -0500

    Update Gallery.php

--- app/Http/Livewire/Creator/Gallery.php
+++ app/Http/Livewire/Creator/Gallery.php
@@ -3,11 +3,47 @@
 namespace App\Http\Livewire\Creator;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Gallery extends Component
 {
+    use WithFileUploads;
+
+    public $file;
+    public $user;
+
+    protected $rules = [
+        'file' => 'required|file|image|max:4096'
+    ];
+
     public function render()
     {
         return view('livewire.creator.gallery');
+    }
+
+    public function updatedFile()
+    {
+        $this->validate();
+        if ($this->user->id != auth()->id()) {
+            $this->addError('file', 'This is not your profile.');
+            return null;
+        }
+        $path = $this->file->store('gallery');
+        $gallery = new \App\Models\Gallery();
+        $gallery->user_id = auth()->id();
+        $gallery->path = $path;
+        $gallery->size = $this->file->getSize();
+        $gallery->type = $this->file->getMimeType();
+        $gallery->save();
+        return redirect()->refresh();
+    }
+    public function delete($id)
+    {
+        $gallery = \App\Models\Gallery::find($id);
+        if ($gallery->user_id != auth()->id()) {
+            $this->addError('file', 'You do not have permission to access this resource.');
+            return null;
+        }
+        $gallery->delete();
     }
 }
