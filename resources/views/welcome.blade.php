f075e2742f2dab07b840223af263ad8118b1f004
Christian Holladay <cholladay0816@gmail.com>
2021-04-03 14:54:02 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -254,25 +254,24 @@
             <div class="relative">
                 <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100"></div>
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+                    <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden z-10">
                         <div class="absolute inset-0">
-                            <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100" alt="People working on laptops">
-                            <div class="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700" style="mix-blend-mode: multiply;"></div>
+                            <div class="absolute inset-0 bg-gradient-to-r from-lightBlue-400 to-lightBlue-500" style="mix-blend-mode: multiply;"></div>
                         </div>
                         <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                             <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
                                 <span class="block text-white">Your all-in-one</span>
-                                <span class="block text-indigo-200">commissioning platform</span>
+                                <span class="block text-lightBlue-200">commissioning platform</span>
                             </h1>
-                            <p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">
+                            <p class="mt-6 max-w-lg mx-auto text-center text-xl text-lightBlue-200 sm:max-w-3xl">
                                 CreatorCore is a commission-based service for digital artists to make money doing what they love.
                             </p>
                             <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                                 <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-                                    <a href="{{ route('register') }}" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8">
+                                    <a href="{{ route('register') }}" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-lightBlue-700 bg-white hover:bg-lightBlue-50 sm:px-8">
                                         Get started
                                     </a>
-                                    <a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8">
+                                    <a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-lightBlue-800 sm:px-8">
                                         Live demo
                                     </a>
                                 </div>
