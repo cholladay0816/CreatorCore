c260b62c91f7a0787e5dbe2928aa28edc1051b69
Christian Holladay <cholladay0816@gmail.com>
2021-04-05 19:37:08 -0500

    Update 2021_01_20_214314_create_sessions_table.php

--- database/migrations/2021_01_20_214314_create_sessions_table.php
+++ database/migrations/2021_01_20_214314_create_sessions_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateSessionsTable extends Migration
@@ -13,6 +14,8 @@ class CreateSessionsTable extends Migration
      */
     public function up()
     {
+        DB::statement('SET SESSION sql_require_primary_key=0');
+
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
