c328baaa14ce2dfc859ac3f45134b8d6af093b0e
Christian Holladay <cholladay0816@gmail.com>
2021-04-10 01:00:40 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Laravel\Jetstream\HasTeams;
 use Laravel\Nova\Actions\Actionable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
