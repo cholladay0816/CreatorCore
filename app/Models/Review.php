52228e01a4652327b5582e2b77cba4328350f4e2
Christian Holladay <cholladay0816@gmail.com>
2021-04-09 23:58:38 -0500

    Update Review.php

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -11,7 +11,7 @@ class Review extends Model
 
     protected $guarded = [];
 
-    public function owner()
+    public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
