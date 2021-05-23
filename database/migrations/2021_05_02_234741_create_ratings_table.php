77e30814a242ba3d6d490fba820db6cee8f70e8a
Christian Holladay <cholladay0816@gmail.com>
2021-05-23 14:40:14 -0500

    Create 2021_05_02_234741_create_ratings_table.php

---
+++ database/migrations/2021_05_02_234741_create_ratings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(\App\Models\Review::class)->constrained()->cascadeOnDelete();
+            $table->tinyInteger('positive')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
