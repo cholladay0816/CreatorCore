4f664f6fcc450690a30a6790d5cc348bf942fa2c
Christian Holladay <cholladay0816@gmail.com>
2021-05-23 15:40:02 -0500

    Update Rating.php

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -9,8 +9,14 @@ class Rating extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
+
     public function review()
     {
         return $this->belongsTo(Review::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
