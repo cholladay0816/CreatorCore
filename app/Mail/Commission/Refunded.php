808d3fee02485ef132eb7d6836d4008105900ff0
Christian Holladay <cholladay0816@gmail.com>
2021-06-27 17:18:21 -0500

    Mail classes

---
+++ app/Mail/Commission/Refunded.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail\Commission;
+
+use App\Models\Commission;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class Refunded extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Commission $commission;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($commission)
+    {
+        $this->commission = $commission;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('mail.commission.refunded', ['commission' => $this->commission])
+            ->subject('Commission Refunded');
+    }
+}
