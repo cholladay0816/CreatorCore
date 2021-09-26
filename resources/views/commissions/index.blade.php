92d2971bb1102f3d2d361b615e71831d067fa6d7
Christian Holladay <cholladay0816@gmail.com>
2021-09-26 13:44:04 -0500

    Add cases for completed and archived commission on index page

--- resources/views/commissions/index.blade.php
+++ resources/views/commissions/index.blade.php
@@ -40,6 +40,28 @@
                                                 <time datetime="{{$commission->expires_at->format("Y-m-d")}}">{{$commission->expires_at->diffForHumans(null, false, true, 2)}}</time>
                                             </p>
                                         </div>
+                                    @elseif($commission->status == 'Completed')
+                                        <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                                            <!-- Heroicon name: solid/calendar -->
+                                            <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+                                            </svg>
+                                            <p>
+                                                Archives in
+                                                <time datetime="{{$commission->expires_at->format("Y-m-d")}}">{{$commission->expires_at->diffForHumans(null, false, true, 2)}}</time>
+                                            </p>
+                                        </div>
+                                    @elseif($commission->status == 'Archived')
+                                        <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                                            <!-- Heroicon name: solid/calendar -->
+                                            <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+                                            </svg>
+                                            <p>
+                                                Completed
+                                                <time datetime="{{$commission->completed_at->format("Y-m-d")}}">{{$commission->completed_at->diffForHumans(null, false, true, 2)}}</time>
+                                            </p>
+                                        </div>
                                     @else
                                         <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
                                             <!-- Heroicon name: solid/calendar -->
