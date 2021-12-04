b02673c98fdc219804bea5312aababef41fdda8e
Christian Holladay <cholladay0816@gmail.com>
2021-12-03 18:39:52 -0600

    Notification Policy

---
+++ app/Policies/NotificationPolicy.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Notification;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Gate;
+
+class NotificationPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Notification  $notification
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Notification $notification)
+    {
+        return $user->id == $notification->user_id || Gate::allows('manage-users');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return Gate::allows('manage-users');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Notification  $notification
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Notification $notification)
+    {
+        return Gate::allows('manage-users');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Notification  $notification
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Notification $notification)
+    {
+        return Gate::allows('manage-users');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Notification  $notification
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Notification $notification)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Notification  $notification
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Notification $notification)
+    {
+        //
+    }
+}
