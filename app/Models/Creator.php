656e323ed06bf06d5bae4d526225a7d2e09897b2
Christian Holladay <cholladay0816@gmail.com>
2022-12-27 19:37:16 -0600

    Allow creators to fetch presets

--- app/Models/Creator.php
+++ app/Models/Creator.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
 
 class Creator extends Model
 {
     use HasFactory;
 
+    public bool $open;
+    public bool $allows_custom_commissions;
+    public User $user;
+    public Collection $commissionPresets;
     protected $guarded = [];
 
     protected $casts = [
@@ -19,5 +25,9 @@ class Creator extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    public function commissionPresets(): HasMany
+    {
+        return $this->hasMany(CommissionPreset::class, 'user_id', 'user_id');
     }
 }
