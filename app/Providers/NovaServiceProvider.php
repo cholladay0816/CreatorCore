d1235f93239f73e66cfbc5bac801275f542643eb
Christian Holladay <cholladay0816@gmail.com>
2022-12-28 10:23:26 -0600

    require user to not be suspended and have permission to view admin menu

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -42,8 +42,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return !$user->suspended();
-            //return $user->hasAbility('view-admin-dashboard');
+            return !$user->suspended() && $user->hasAbility('view-admin-dashboard');
         });
     }
 
