b1d00c6e524a7e13ea128f6cc6e2360e9e629430
Christian Holladay <cholladay0816@gmail.com>
2023-04-05 19:43:38 -0500

    Adjust global scope for commissionevent

--- app/Models/CommissionEvent.php
+++ app/Models/CommissionEvent.php
@@ -18,6 +18,6 @@ class CommissionEvent extends Model
 
     protected static function booted()
     {
-        static::addGlobalScope(EventForScope::class);
+        static::addGlobalScope(new EventForScope);
     }
 }
