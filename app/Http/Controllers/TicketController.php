fdc164c97ac0be6b7af2cd4892beaea5a8d1304a
Christian Holladay <cholladay0816@gmail.com>
2023-08-22 19:00:45 -0500

    Ticket mailer event

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\Ticket\Submitted;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 
@@ -10,7 +11,7 @@ class TicketController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
@@ -20,22 +21,32 @@ class TicketController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
-        //
+        return view('tickets.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
     {
-        //
+        $res = $request->validate([
+            'title' => 'max:256',
+            'description' => 'max:1024'
+        ]);
+        $ticket = Ticket::create([
+            'user_id' => auth()->id(),
+            'title' => $res['title'],
+            'description' => $res['description']
+        ]);
+        Submitted::dispatch($ticket);
+        return redirect()->route('tickets.index');
     }
 
     /**
