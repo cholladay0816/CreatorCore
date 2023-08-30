89f606dcf8f02f9f06a0f9762053ba8b2aa0dfe8
Christian Holladay <cholladay0816@gmail.com>
2023-08-29 23:03:11 -0500

    display affiliate incentive and percentage data on dashboard

--- resources/views/livewire/dashboard-stats.blade.php
+++ resources/views/livewire/dashboard-stats.blade.php
@@ -88,7 +88,7 @@
                     </svg>
 
                 </div>
-                <p class="ml-16 text-sm font-medium text-gray-500 truncate">Bonus Payments</p>
+                <p class="ml-16 text-sm font-medium text-gray-500 truncate">Incentive Payments</p>
             </dt>
             <dd class="ml-16 flex items-baseline">
                 <p class="text-2xl font-semibold text-gray-900">
@@ -104,7 +104,7 @@
                     </svg>
 
                 </div>
-                <p class="ml-16 text-sm font-medium text-gray-500 truncate">Bonus credited</p>
+                <p class="ml-16 text-sm font-medium text-gray-500 truncate">Incentive credited</p>
             </dt>
             <dd class="ml-16 flex items-baseline">
                 <p class="text-2xl font-semibold text-gray-900">
@@ -120,7 +120,7 @@
                     </svg>
 
                 </div>
-                <p class="ml-16 text-sm font-medium text-gray-500 truncate">Bonus remaining</p>
+                <p class="ml-16 text-sm font-medium text-gray-500 truncate">Inventive remaining</p>
             </dt>
             <dd class="ml-16 flex items-baseline">
                 <p class="text-2xl font-semibold text-gray-900">
@@ -145,6 +145,42 @@
                 </span>
             </dd>
         </div>
+        @if($affiliate->percentage)
+            <div class="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
+                <dt>
+                    <div class="absolute bg-indigo-500 rounded-md p-3">
+                        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+
+                    </div>
+                    <p class="ml-16 text-sm font-medium text-gray-500 truncate">Affiliate Percentage</p>
+                </dt>
+                <dd class="ml-16 flex items-baseline">
+                    <p class="text-2xl font-semibold text-gray-900">
+                        {{ $affiliate->percentage * 100 }}%
+                    </p>
+                </dd>
+            </div>
+            @endif
+            @if($affiliate->incentive_amount)
+            <div class="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
+                <dt>
+                    <div class="absolute bg-indigo-500 rounded-md p-3">
+                        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+
+                    </div>
+                    <p class="ml-16 text-sm font-medium text-gray-500 truncate">Incentive-Per-Commission</p>
+                </dt>
+                <dd class="ml-16 flex items-baseline">
+                    <p class="text-2xl font-semibold text-gray-900">
+                        ${{ number_format((float)$affiliate->incentive_amount / 100, 2) }}
+                    </p>
+                </dd>
+            </div>
+            @endif
         @endif
     </dl>
 </div>
