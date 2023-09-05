11adfbd7b653c752f0f35908fdc26fe62a773a10
Christian Holladay <cholladay0816@gmail.com>
2023-09-05 18:58:00 -0500

    Redirect back button

--- resources/views/commissionpresets/create.blade.php
+++ resources/views/commissionpresets/create.blade.php
@@ -92,9 +92,9 @@
 
                 <div class="pt-5">
                     <div class="flex justify-end">
-                        <button type="button" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <a href="{{ redirect()->back()->getTargetUrl() }}" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             Cancel
-                        </button>
+                        </a>
                         <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             Save
                         </button>
