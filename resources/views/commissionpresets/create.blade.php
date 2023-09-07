c552d05e50dbc14b4e7ea98f061f05101f18df8c
Christian Holladay <cholladay0816@gmail.com>
2023-09-07 16:45:39 -0500

    delete button for presets

--- resources/views/commissionpresets/create.blade.php
+++ resources/views/commissionpresets/create.blade.php
@@ -115,12 +115,24 @@
                         <a href="{{ redirect()->back()->getTargetUrl() }}" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             Cancel
                         </a>
+                        @if(isset($commissionPreset))
+                            <button type="submit" form="deleteform" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                                Delete
+                            </button>
+                        @endif
                         <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             Save
                         </button>
                     </div>
                 </div>
+            </div>
             </form>
+            @if(isset($commissionPreset))
+            <form id="deleteform" method="post" action="{{ route('commissionpresets.destroy', $commissionPreset) }}">
+                @csrf
+                @method('DELETE')
+            </form>
+            @endif
         </div>
     </div>
 
