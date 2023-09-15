5a2f81e5d596e89a142c126c1a1dbae3cfe480ff
Christian Holladay <cholladay0816@gmail.com>
2023-09-15 13:56:15 -0500

    Padding on skip and finish buttons

--- resources/views/livewire/onboarding/buyer-finish.blade.php
+++ resources/views/livewire/onboarding/buyer-finish.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="px-2 sm:px-0">
     <div>
         <button class="bg-indigo-500 text-white px-5 py-3 rounded-lg font-bold" wire:click="onboard">Finish</button>
     </div>
--- resources/views/livewire/onboarding/creator-skip.blade.php
+++ resources/views/livewire/onboarding/creator-skip.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="px-2 sm:px-0">
     <div>
         <button class="border border-indigo-500 hover:bg-indigo-400 hover:text-white focus:bg-indigo-500 text-gray-700 px-4 py-2 rounded-lg font-italic" wire:click="skip">Skip</button>
     </div>
