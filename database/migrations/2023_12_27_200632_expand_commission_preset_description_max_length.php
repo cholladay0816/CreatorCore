ff0f243d9f8250ab265304f975d1ac8e91f99fa0
cholladay0816 <cholladay0816@users.noreply.github.com>
2023-12-27 20:10:29 +0000

    Apply php-cs-fixer changes

--- database/migrations/2023_12_27_200632_expand_commission_preset_description_max_length.php
+++ database/migrations/2023_12_27_200632_expand_commission_preset_description_max_length.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,11 +12,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('commission_presets', function(Blueprint $table) {
+        Schema::table('commission_presets', function (Blueprint $table) {
             $table->text('description')->change();
         });
 
-        Schema::table('commissions', function(Blueprint $table) {
+        Schema::table('commissions', function (Blueprint $table) {
             $table->text('description')->change();
         });
     }
