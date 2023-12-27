c296140090a5d1294862f8a1c3bfc0263d79a018
Christian Holladay <cholladay0816@gmail.com>
2023-12-27 13:31:22 -0600

    hide update password if no password set

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -13,7 +13,7 @@
                 <x-jet-section-border />
             @endif
 
-            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()) && $user->passwordSet())
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.update-password-form')
                 </div>
