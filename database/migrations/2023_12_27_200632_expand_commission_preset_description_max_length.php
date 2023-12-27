c3a6cd5c62fdc6360593a7fc26de5c755678d8d6
Christian Holladay <cholladay0816@gmail.com>
2023-12-27 14:09:48 -0600

    Raise commission and preset character limit

---
+++ database/migrations/2023_12_27_200632_expand_commission_preset_description_max_length.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('commission_presets', function(Blueprint $table) {
+            $table->text('description')->change();
+        });
+
+        Schema::table('commissions', function(Blueprint $table) {
+            $table->text('description')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
